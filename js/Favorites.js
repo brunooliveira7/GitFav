81d9496ed2b2a9ef8ad3ce3c33a3273ae85e0f90
Bruno Oliveira <brunoroliveira7@gmail.com>
2024-10-03 20:06:17 -0300

    table tbody

--- js/Favorites.js
+++ js/Favorites.js
@@ -1,16 +1,16 @@
 export class Favorites {
-  //o root é a #app
   constructor(root) {
-    //colocando a #app e colocando dentro do this.root
     this.root = document.querySelector(root);
   }
 }
 
 export class FavoritesView extends Favorites {
-  //a leitura sai do main.js e vem para cá - o rott = #app
   constructor(root) {
-    //super - passa o root para dentro, linha que liga as duas classes
     super(root);
-    console.log(root)
+
+    this.tbody = this.root.querySelector("table tbody");
+
+   
+
   }
 }
