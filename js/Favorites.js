b074adf0f80cf2cc245155c3a01eb54c4a7a2bfa
Bruno Oliveira <brunoroliveira7@gmail.com>
2024-10-04 10:05:54 -0300

    starts creating lines

--- js/Favorites.js
+++ js/Favorites.js
@@ -3,7 +3,14 @@ import { GithubUser } from "./GithubUser.js";
 export class Favorites {
   constructor(root) {
     this.root = document.querySelector(root);
+
+    this.load();
+
+    GithubUser.seach("brunooliveira7").then((user) => console.log(user));
   }
+
+
+  
 }
 
 export class FavoritesView extends Favorites {
@@ -13,20 +20,63 @@ export class FavoritesView extends Favorites {
     this.tbody = this.root.querySelector("table tbody");
 
     this.upDate();
-
   }
 
   upDate() {
-
     this.removeAllTr();
+
+    this.entries.forEach((user) => {
+      const row = this.createRow();
+
+      row.querySelector(
+        ".user img"
+      ).src = `https://github.com/${user.login}.png`;
+      row.querySelector(".user img").alt = `Imagem de ${user.name}`;
+      row.querySelector(".user a").href = `https://github.com/${user.login}`;
+      row.querySelector(".user p").textContent = user.name;
+      row.querySelector(".user span").textContent = user.login;
+      row.querySelector(".repositories").textContent = user.public_repos;
+      row.querySelector(".followers").textContent = user.followers;
+
+      row.querySelector(".remove").onclick = () => {
+        const isOk = confirm("Tem certeza que deseja deletar essa linha?");
+        if (isOk) {
+          this.delete(user);
+        }
+      };
+
+      this.tbody.append(row);
+    });
   }
 
+  createRow() {
+    const tr = document.createElement("tr");
+
+    tr.innerHTML = `
+            <td class="user">
+              <img
+                src="https://github.com/brunooliveira7.png"
+                alt="Imagem de brunooliveira7"
+              />
+              <a href="https://github.com/brunooliveira7" target="_blank">
+                <p>Bruno Oliveira</p>
+                <span>brunooliveira7</span>
+              </a>
+            </td>
+            <td class="repositories">76</td>
+            <td class="followers">9589</td>
+            <td>
+              <button class="remove">&times;</button>
+            </td>
+        `;
+    return tr;
+  }
 
   removeAllTr() {
-    //retira as linhas do body
     this.tbody.querySelectorAll("tr").forEach((tr) => {
       tr.remove();
     });
   }
+
 
 }
